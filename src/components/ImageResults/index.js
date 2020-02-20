7d738a96780ef374340509e952f501b7d8d7b84f
maxgdev <max@apphilia.net>
2020-02-20 11:39:51 +0000

    Added Search, NavBar & ImageResults components

---
+++ src/components/ImageResults/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ImageResults extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Image Results</h1>
+            </div>
+        )
+    }
+}
+export default ImageResults;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { AppBar } from 'material-ui'
+
+function NavBar() {
+    return (
+        <AppBar title="React Pixabay Image Finder App"/>
+    )
+}
+export default  NavBar;
