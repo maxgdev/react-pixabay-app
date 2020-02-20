99b676613f0339e97283b5ac6ddf148babab5099
maxgdev <max@apphilia.net>
2020-02-20 11:53:42 +0000

    Added material-ui TextField and SelectField to Search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import TextField            from 'material-ui/TextField';
+import SelectField          from 'material-ui/SelectField';
+import MenuItem             from 'material-ui/MenuItem';
+import axios                from 'axios';
+// import ImageResults         from '../ImageResults';
 
 class Search extends Component {
+
     render() {
         return (
             <div>
-                <h1 >Search Component</h1>
+                <TextField />
+                <br />
+                <SelectField>
+                    <MenuItem value={5} primaryText="5"/>
+                    <MenuItem value={10} primaryText="10"/>
+                    <MenuItem value={15} primaryText="15"/>
+                    <MenuItem value={30} primaryText="30"/>
+                    <MenuItem value={50} primaryText="50"/>
+                </SelectField>
+                <br />
+                <p>Results go here</p>
             </div>
         )
     }
