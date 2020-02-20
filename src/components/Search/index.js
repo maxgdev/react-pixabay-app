e4269a22e833fef0f2d33b08f715f2f2b4ec69c3
maxgdev <max@apphilia.net>
2020-02-20 12:15:37 +0000

    Added full code to Search component

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -7,12 +7,52 @@ import axios                from 'axios';
 
 class Search extends Component {
 
+    state = {
+        searchText: "",
+        amount: 15,
+        images: [],
+        apiUrl: 'https://pixabay.com/api',
+        apikey: '',
+    }
+
+    onTextChange = (e) => {
+        const val = e.target.value;
+        this.setState(
+            { searchText: e.target.value}, 
+            ()=> {
+                if (val === '') {
+                    this.setState({images: []});
+                } else {
+                    axios.get(`${this.state.apiUrl}/?key=${this.state.apikey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+                    .then(res => this.setState({ images: res.data.hits}))
+                    .catch( err => console.log(err));
+                }
+        });
+
+    };
+
+    onAmountChange = (e, index, value) => {
+        this.setState({ amount: value});
+    };
+
     render() {
+        console.log(this.state);
         return (
             <div>
-                <TextField />
+                <TextField 
+                    name="SearchText"
+                    floatingLabelText="Type here to search for Images"
+                    fullWidth={true}
+                    value={this.state.searchText}
+                    onChange={this.onTextChange}
+                />
                 <br />
-                <SelectField>
+                <SelectField
+                    name="amount"
+                    floatingLabelText="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                >
                     <MenuItem value={5} primaryText="5"/>
                     <MenuItem value={10} primaryText="10"/>
                     <MenuItem value={15} primaryText="15"/>
